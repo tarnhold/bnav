f0e6f905da6ea2482eb8efeaf6e63b6469695972
Thomas Arnhold <thomas@arnhold.org>
2014-07-24 19:20:11 +0200

    add main application

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
-#include "NavBits.h"
+#include "AsciiReader.h"
 
-#include <iostream>
-
-using namespace std;
-
-int main()
+static void usage()
 {
-    bnav::NavBits<20> bits;
+    std::cout << "usage: bnav [FILENAME]" << std::endl;
+}
 
-    cout << "Hello World!" << endl;
+int main(int argc, char **argv)
+{
+    // require at least one parameter
+    if (argc != 2)
+    {
+        usage();
+        return 1;
+    }
+
+    // Open file and parse lines
+    bnav::AsciiReader reader(argv[1], "sbf");
+    if (!reader.isOpen())
+    {
+        std::cerr << "Could not open file: " << argv[1] << std::endl;
+        return 0;
+    }
+
+    bnav::ReaderNavEntry data;
+    while (reader.readLine(data))
+    {
+        std::cout << "prn: " << data.getPRN() << " tow: " << data.getTOW() << std::endl;
+    }
+    reader.close();
+
+
     return 0;
 }
 
