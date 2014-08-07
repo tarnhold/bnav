f8a9032e2dc8ae17e9761c42a0e2476bab5aa6e0
Thomas Arnhold <thomas@arnhold.org>
2014-08-07 21:53:42 +0200

    tests: organize as suite

--- tests/testSubframe.cpp
+++ tests/testSubframe.cpp
@@ -75,249 +75,255 @@ TEST(testSubframeConstructor)
 #endif
 
 // Test real data
-TEST(testSubframe_SimpleD1)
+SUITE(testSubframe_SBF_Simple)
 {
-    std::stringstream ssfile;
-    ssfile << PATH_TESTDATA << "sbf/prn6-fraID.txt";
-
-    bnav::AsciiReader reader;
-    reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    reader.open(ssfile.str());
-    CHECK(reader.isOpen());
-
-    const uint32_t sowlist[] = {345600, 345606, 345612, 345618, 345624};
-
-    std::size_t i = 0;
-    bnav::ReaderNavEntry entry;
-    while (reader.readLine(entry))
+    TEST(testSubframe_SimpleD1)
     {
-        if (entry.getSignalType() != bnav::SignalType::BDS_B1)
-            continue;
+        std::stringstream ssfile;
+        ssfile << PATH_TESTDATA << "sbf/prn6-fraID.txt";
 
-        bnav::SvID sv(entry.getPRN());
-        CHECK(!sv.isGeo());
-        bnav::Subframe sf(entry.getTOW(), entry.getBits(), sv.isGeo());
+        bnav::AsciiReader reader;
+        reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        reader.open(ssfile.str());
+        CHECK(reader.isOpen());
 
-        CHECK(sf.getFrameID() == i + 1);
+        const uint32_t sowlist[] = {345600, 345606, 345612, 345618, 345624};
 
-        // non-Geo: FraID 1-3 have no Pnum
-        if (sf.getFrameID() < 4)
-            CHECK(sf.getPageNum() == 0);
-        else
-            CHECK(sf.getPageNum() == 1);
-
-        CHECK(sf.getSOW() == sowlist[i]);
-
-        ++i;
-    }
-    CHECK(i == 5);
-}
-
-// same as testSubframeFraIDSimpleD1, but with empty constructor
-TEST(testSubframe_SimpleD1_empty)
-{
-    std::stringstream ssfile;
-    ssfile << PATH_TESTDATA << "sbf/prn6-fraID.txt";
-
-    bnav::AsciiReader reader;
-    reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    reader.open(ssfile.str());
-    CHECK(reader.isOpen());
-
-    const uint32_t sowlist[] = {345600, 345606, 345612, 345618, 345624};
-
-    std::size_t i = 0;
-    bnav::ReaderNavEntry entry;
-    while (reader.readLine(entry))
-    {
-        if (entry.getSignalType() != bnav::SignalType::BDS_B1)
-            continue;
-
-        bnav::SvID sv(entry.getPRN());
-        CHECK(!sv.isGeo());
-
-        // only difference
-        bnav::Subframe sf;
-        sf.setTOW(entry.getTOW());
-        sf.setBits(entry.getBits());
-        sf.setGeo(sv.isGeo());
-        sf.initialize();
-
-        CHECK(sf.getFrameID() == i + 1);
-
-        // non-Geo: FraID 1-3 have no Pnum
-        if (sf.getFrameID() < 4)
-            CHECK(sf.getPageNum() == 0);
-        else
-            CHECK(sf.getPageNum() == 1);
-
-        CHECK(sf.getSOW() == sowlist[i]);
-
-        ++i;
-    }
-    CHECK(i == 5);
-}
-
-TEST(testSubframe_SimpleD2)
-{
-    std::stringstream ssfile;
-    ssfile << PATH_TESTDATA << "sbf/prn2-fraID.txt";
-
-    bnav::AsciiReader reader;
-    reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    reader.open(ssfile.str());
-    CHECK(reader.isOpen());
-
-    std::size_t i = 0;
-    bnav::ReaderNavEntry entry;
-    while (reader.readLine(entry))
-    {
-        if (entry.getSignalType() != bnav::SignalType::BDS_B1)
-            continue;
-
-        bnav::SvID sv(entry.getPRN());
-        CHECK(sv.isGeo());
-        bnav::Subframe sf(entry.getTOW(), entry.getBits(), sv.isGeo());
-
-        CHECK(sf.getFrameID() == i + 1);
-
-        // Geo: FraID 3 and 4 have no Pnum
-        if (sf.getFrameID() == 3 || sf.getFrameID() == 4)
-            CHECK(sf.getPageNum() == 0);
-        else
-            CHECK(sf.getPageNum() == 1);
-
-        // Geo: have same SOW for FraID 1-5
-        CHECK(sf.getSOW() == 345600);
-
-        ++i;
-    }
-    CHECK(i == 5);
-}
-
-TEST(testSubframe_OneFrameD1)
-{
-    std::stringstream ssfile;
-    ssfile << PATH_TESTDATA << "sbf/prn6-oneframe.txt";
-
-    bnav::AsciiReader reader;
-    reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    reader.open(ssfile.str());
-    CHECK(reader.isOpen());
-
-    const uint32_t sowfirst = 346320;
-
-    std::size_t i = 0;
-    std::size_t pnum_fra4 = 0;
-    std::size_t pnum_fra5 = 0;
-    bnav::ReaderNavEntry entry;
-    while (reader.readLine(entry))
-    {
-        if (entry.getSignalType() != bnav::SignalType::BDS_B1)
-            continue;
-
-        bnav::SvID sv(entry.getPRN());
-        CHECK(!sv.isGeo());
-        bnav::Subframe sf(entry.getTOW(), entry.getBits(), sv.isGeo());
-
-        CHECK(sf.getFrameID() == (i % 5) + 1);
-
-        // check Pnum for frame 4 and 5
-        if (sf.getFrameID() == 4)
+        std::size_t i = 0;
+        bnav::ReaderNavEntry entry;
+        while (reader.readLine(entry))
         {
-            ++pnum_fra4;
-            CHECK(sf.getPageNum() == pnum_fra4);
-        }
-        else if (sf.getFrameID() == 5)
-        {
-            ++pnum_fra5;
-            CHECK(sf.getPageNum() == pnum_fra5);
-        }
-        else
+            if (entry.getSignalType() != bnav::SignalType::BDS_B1)
+                continue;
+
+            bnav::SvID sv(entry.getPRN());
+            CHECK(!sv.isGeo());
+            bnav::Subframe sf(entry.getTOW(), entry.getBits(), sv.isGeo());
+
+            CHECK(sf.getFrameID() == i + 1);
+
             // non-Geo: FraID 1-3 have no Pnum
-            CHECK(sf.getPageNum() == 0);
+            if (sf.getFrameID() < 4)
+                CHECK(sf.getPageNum() == 0);
+            else
+                CHECK(sf.getPageNum() == 1);
 
-        // subframes come every 6s
-        CHECK(sf.getSOW() == sowfirst + i * 6);
+            CHECK(sf.getSOW() == sowlist[i]);
 
-        ++i;
+            ++i;
+        }
+        CHECK(i == 5);
     }
-    CHECK(i == 24*5);
-    CHECK(pnum_fra4 == 24);
-    CHECK(pnum_fra5 == 24);
+
+    // same as testSubframeFraIDSimpleD1, but with empty constructor
+    TEST(testSubframe_SimpleD1_empty)
+    {
+        std::stringstream ssfile;
+        ssfile << PATH_TESTDATA << "sbf/prn6-fraID.txt";
+
+        bnav::AsciiReader reader;
+        reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        reader.open(ssfile.str());
+        CHECK(reader.isOpen());
+
+        const uint32_t sowlist[] = {345600, 345606, 345612, 345618, 345624};
+
+        std::size_t i = 0;
+        bnav::ReaderNavEntry entry;
+        while (reader.readLine(entry))
+        {
+            if (entry.getSignalType() != bnav::SignalType::BDS_B1)
+                continue;
+
+            bnav::SvID sv(entry.getPRN());
+            CHECK(!sv.isGeo());
+
+            // only difference
+            bnav::Subframe sf;
+            sf.setTOW(entry.getTOW());
+            sf.setBits(entry.getBits());
+            sf.setGeo(sv.isGeo());
+            sf.initialize();
+
+            CHECK(sf.getFrameID() == i + 1);
+
+            // non-Geo: FraID 1-3 have no Pnum
+            if (sf.getFrameID() < 4)
+                CHECK(sf.getPageNum() == 0);
+            else
+                CHECK(sf.getPageNum() == 1);
+
+            CHECK(sf.getSOW() == sowlist[i]);
+
+            ++i;
+        }
+        CHECK(i == 5);
+    }
+
+    TEST(testSubframe_SimpleD2)
+    {
+        std::stringstream ssfile;
+        ssfile << PATH_TESTDATA << "sbf/prn2-fraID.txt";
+
+        bnav::AsciiReader reader;
+        reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        reader.open(ssfile.str());
+        CHECK(reader.isOpen());
+
+        std::size_t i = 0;
+        bnav::ReaderNavEntry entry;
+        while (reader.readLine(entry))
+        {
+            if (entry.getSignalType() != bnav::SignalType::BDS_B1)
+                continue;
+
+            bnav::SvID sv(entry.getPRN());
+            CHECK(sv.isGeo());
+            bnav::Subframe sf(entry.getTOW(), entry.getBits(), sv.isGeo());
+
+            CHECK(sf.getFrameID() == i + 1);
+
+            // Geo: FraID 3 and 4 have no Pnum
+            if (sf.getFrameID() == 3 || sf.getFrameID() == 4)
+                CHECK(sf.getPageNum() == 0);
+            else
+                CHECK(sf.getPageNum() == 1);
+
+            // Geo: have same SOW for FraID 1-5
+            CHECK(sf.getSOW() == 345600);
+
+            ++i;
+        }
+        CHECK(i == 5);
+    }
 }
 
-TEST(testSubframe_OneFrameD2)
+SUITE(testSubframe_SBF_OneFrame)
 {
-    std::stringstream ssfile;
-    ssfile << PATH_TESTDATA << "sbf/prn2-oneframe.txt";
-
-    bnav::AsciiReader reader;
-    reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
-    reader.open(ssfile.str());
-    CHECK(reader.isOpen());
-
-    // set first sow
-    uint32_t sow = 365040;
-
-    std::size_t i = 0;
-    std::size_t pnum_fra1 = 0;
-    std::size_t pnum_fra2 = 0;
-    std::size_t pnum_fra5 = 0;
-    bnav::ReaderNavEntry entry;
-    while (reader.readLine(entry))
+    TEST(testSubframe_OneFrameD1)
     {
-        if (entry.getSignalType() != bnav::SignalType::BDS_B1)
-            continue;
+        std::stringstream ssfile;
+        ssfile << PATH_TESTDATA << "sbf/prn6-oneframe.txt";
 
-        bnav::SvID sv(entry.getPRN());
-        CHECK(sv.isGeo());
-        bnav::Subframe sf(entry.getTOW(), entry.getBits(), sv.isGeo());
+        bnav::AsciiReader reader;
+        reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        reader.open(ssfile.str());
+        CHECK(reader.isOpen());
 
-        CHECK(sf.getFrameID() == (i % 5) + 1);
-        CHECK(sf.getSOW() == sow);
+        const uint32_t sowfirst = 346320;
 
-        // check Pnum for frame 1, 2 and 5
-        if (sf.getFrameID() == 1)
+        std::size_t i = 0;
+        std::size_t pnum_fra4 = 0;
+        std::size_t pnum_fra5 = 0;
+        bnav::ReaderNavEntry entry;
+        while (reader.readLine(entry))
         {
-            // reset after 10 pages
-            if (pnum_fra1 == 10)
-                pnum_fra1 = 0;
-            ++pnum_fra1;
-            CHECK(sf.getPageNum() == pnum_fra1);
-        }
-        else if (sf.getFrameID() == 2)
-        {
-            // reset after 6 pages
-            if (pnum_fra2 == 6)
-                pnum_fra2 = 0;
-            ++pnum_fra2;
-            CHECK(sf.getPageNum() == pnum_fra2);
-        }
-        else if (sf.getFrameID() == 5)
-        {
-            ++pnum_fra5;
-            CHECK(sf.getPageNum() == pnum_fra5);
+            if (entry.getSignalType() != bnav::SignalType::BDS_B1)
+                continue;
 
-            // increment SOW if we hit frame 5
-            // with D2 we have subframes every 0.6s which is 0.6s * 5 = 3s
-            // BeiDou transmits a new SOW on every FraID == 1 for D2
-            sow += 3;
-        }
-        else
-            // Geo: FraID 3 and 4 have no Pnum
-            CHECK(sf.getPageNum() == 0);
+            bnav::SvID sv(entry.getPRN());
+            CHECK(!sv.isGeo());
+            bnav::Subframe sf(entry.getTOW(), entry.getBits(), sv.isGeo());
 
-        ++i;
+            CHECK(sf.getFrameID() == (i % 5) + 1);
+
+            // check Pnum for frame 4 and 5
+            if (sf.getFrameID() == 4)
+            {
+                ++pnum_fra4;
+                CHECK(sf.getPageNum() == pnum_fra4);
+            }
+            else if (sf.getFrameID() == 5)
+            {
+                ++pnum_fra5;
+                CHECK(sf.getPageNum() == pnum_fra5);
+            }
+            else
+                // non-Geo: FraID 1-3 have no Pnum
+                CHECK(sf.getPageNum() == 0);
+
+            // subframes come every 6s
+            CHECK(sf.getSOW() == sowfirst + i * 6);
+
+            ++i;
+        }
+        CHECK(i == 24*5);
+        CHECK(pnum_fra4 == 24);
+        CHECK(pnum_fra5 == 24);
     }
-    CHECK(i == 120*5);
-    CHECK(pnum_fra1 == 10);
-    CHECK(pnum_fra2 == 6);
-    CHECK(pnum_fra5 == 120);
-    CHECK(sow == 365397 + 3); // because we incremented one too much
+
+    TEST(testSubframe_OneFrameD2)
+    {
+        std::stringstream ssfile;
+        ssfile << PATH_TESTDATA << "sbf/prn2-oneframe.txt";
+
+        bnav::AsciiReader reader;
+        reader.setType(bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        CHECK(reader.getType() == bnav::AsciiReaderType::TEXT_CONVERTED_SBF);
+        reader.open(ssfile.str());
+        CHECK(reader.isOpen());
+
+        // set first sow
+        uint32_t sow = 365040;
+
+        std::size_t i = 0;
+        std::size_t pnum_fra1 = 0;
+        std::size_t pnum_fra2 = 0;
+        std::size_t pnum_fra5 = 0;
+        bnav::ReaderNavEntry entry;
+        while (reader.readLine(entry))
+        {
+            if (entry.getSignalType() != bnav::SignalType::BDS_B1)
+                continue;
+
+            bnav::SvID sv(entry.getPRN());
+            CHECK(sv.isGeo());
+            bnav::Subframe sf(entry.getTOW(), entry.getBits(), sv.isGeo());
+
+            CHECK(sf.getFrameID() == (i % 5) + 1);
+            CHECK(sf.getSOW() == sow);
+
+            // check Pnum for frame 1, 2 and 5
+            if (sf.getFrameID() == 1)
+            {
+                // reset after 10 pages
+                if (pnum_fra1 == 10)
+                    pnum_fra1 = 0;
+                ++pnum_fra1;
+                CHECK(sf.getPageNum() == pnum_fra1);
+            }
+            else if (sf.getFrameID() == 2)
+            {
+                // reset after 6 pages
+                if (pnum_fra2 == 6)
+                    pnum_fra2 = 0;
+                ++pnum_fra2;
+                CHECK(sf.getPageNum() == pnum_fra2);
+            }
+            else if (sf.getFrameID() == 5)
+            {
+                ++pnum_fra5;
+                CHECK(sf.getPageNum() == pnum_fra5);
+
+                // increment SOW if we hit frame 5
+                // with D2 we have subframes every 0.6s which is 0.6s * 5 = 3s
+                // BeiDou transmits a new SOW on every FraID == 1 for D2
+                sow += 3;
+            }
+            else
+                // Geo: FraID 3 and 4 have no Pnum
+                CHECK(sf.getPageNum() == 0);
+
+            ++i;
+        }
+        CHECK(i == 120*5);
+        CHECK(pnum_fra1 == 10);
+        CHECK(pnum_fra2 == 6);
+        CHECK(pnum_fra5 == 120);
+        CHECK(sow == 365397 + 3); // because we incremented one too much
+    }
 }
