76af828dd7975f4f5073f8326e1897c024acba5b
Thomas Arnhold <thomas@arnhold.org>
2014-08-15 15:30:49 +0200

    Benchmark: add to project

---
+++ lib/Benchmark.cpp
@@ -0,0 +1,36 @@
+#include "Benchmark.h"
+
+#include <iostream>
+
+/*
+ * simple benchmarking
+ *
+ * text is a description which is used within end()
+ */
+Benchmark::Benchmark(const std::string &text)
+	: m_text(text)
+	, m_begin()
+	, m_end()
+{
+	begin();
+}
+
+Benchmark::~Benchmark()
+{
+}
+
+void Benchmark::begin()
+{
+	m_begin = std::chrono::high_resolution_clock::now();
+}
+
+void Benchmark::end()
+{
+	m_end = std::chrono::high_resolution_clock::now();
+
+	std::cout << m_text << ": "
+		<< std::chrono::duration_cast<
+			std::chrono::milliseconds>(m_end-m_begin).count()
+		<< " ms" << std::endl;
+}
+
---
+++ lib/Benchmark.h
@@ -0,0 +1,38 @@
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+#include <chrono>
+#include <cstdint>
+#include <string>
+
+#define BENCH_LOOP(count) for (uint64_t bench_i = 0; bench_i < count; ++bench_i)
+
+/**
+ * @brief The Benchmark class
+ *
+ * Usage example:
+ *
+ * Benchmark bench("asciireader");
+ * BENCH_LOOP(10000)
+ * {
+ *     // do something
+ * }
+ * bench.end();
+ */
+
+class Benchmark
+{
+	std::string m_text;
+	std::chrono::high_resolution_clock::time_point m_begin;
+	std::chrono::high_resolution_clock::time_point m_end;
+
+public:
+	Benchmark(const std::string &text);
+	~Benchmark();
+
+	void begin();
+	void end();
+
+};
+
+#endif // BENCHMARK_H
