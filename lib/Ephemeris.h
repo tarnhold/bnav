ba039ebf030532d4952fd226405bf7cff49e70bc
Thomas Arnhold <thomas@arnhold.org>
2014-08-28 12:17:45 +0200

    Ephemeris: dump and diff KlobucharParam

--- lib/Ephemeris.h
+++ lib/Ephemeris.h
@@ -32,6 +32,21 @@ struct KlobucharParam
     {
     }
 
+    KlobucharParam operator-(const KlobucharParam &rhs)
+    {
+        KlobucharParam ret;
+        ret.alpha0 = std::fabs(alpha0 - rhs.alpha0);
+        ret.alpha1 = std::fabs(alpha1 - rhs.alpha1);
+        ret.alpha2 = std::fabs(alpha2 - rhs.alpha2);
+        ret.alpha3 = std::fabs(alpha3 - rhs.alpha3);
+        ret.beta0 = std::fabs(beta0 - rhs.beta0);
+        ret.beta1 = std::fabs(beta1 - rhs.beta1);
+        ret.beta2 = std::fabs(beta2 - rhs.beta2);
+        ret.beta3 = std::fabs(beta3 - rhs.beta3);
+
+        return ret;
+    }
+
 #if 0
     // floating point comparisons...
     bool operator==(const KlobucharParam &rhs)
@@ -79,6 +94,25 @@ private:
     void processD2Page1(const Subframe &sf);
     void processD2Page2(const Subframe &sf);
 };
+
+// non-members
+std::ostream & operator<<(std::ostream & out, const KlobucharParam & rhs)
+{
+    const std::size_t width = 10;
+    out << std::setprecision(4);
+    out << "alpha: "
+        << std::setw(width) << rhs.alpha0 << " "
+        << std::setw(width) << rhs.alpha1 << " "
+        << std::setw(width) << rhs.alpha2 << " "
+        << std::setw(width) << rhs.alpha3;
+    out << std::endl;
+    out << " beta: "
+        << std::setw(width) << rhs.beta0 << " "
+        << std::setw(width) << rhs.beta1 << " "
+        << std::setw(width) << rhs.beta2 << " "
+        << std::setw(width) << rhs.beta3;
+    return out;
+}
 
 } // namespace bnav
 
