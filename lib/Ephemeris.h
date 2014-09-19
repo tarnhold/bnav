8a20b408a7b01c1f5f861bcefd2d368d82dfd781
Thomas Arnhold <thomas@arnhold.org>
2014-09-19 15:52:45 +0200

    Ephemeris: move KlobucharParam implementation out of the header

--- lib/Ephemeris.h
+++ lib/Ephemeris.h
@@ -21,49 +21,12 @@ struct KlobucharParam
     double beta2;
     double beta3;
 
-    KlobucharParam()
-        : rawbits(0)
-        , alpha0(0.0)
-        , alpha1(0.0)
-        , alpha2(0.0)
-        , alpha3(0.0)
-        , beta0(0.0)
-        , beta1(0.0)
-        , beta2(0.0)
-        , beta3(0.0)
-    {
-    }
+    KlobucharParam();
 
-    KlobucharParam operator-(const KlobucharParam &rhs)
-    {
-        KlobucharParam ret;
-        // rawbits are zero by default, leave it this way
-        ret.alpha0 = std::fabs(alpha0 - rhs.alpha0);
-        ret.alpha1 = std::fabs(alpha1 - rhs.alpha1);
-        ret.alpha2 = std::fabs(alpha2 - rhs.alpha2);
-        ret.alpha3 = std::fabs(alpha3 - rhs.alpha3);
-        ret.beta0 = std::fabs(beta0 - rhs.beta0);
-        ret.beta1 = std::fabs(beta1 - rhs.beta1);
-        ret.beta2 = std::fabs(beta2 - rhs.beta2);
-        ret.beta3 = std::fabs(beta3 - rhs.beta3);
+    KlobucharParam operator-(const KlobucharParam &rhs);
 
-        return ret;
-    }
-
-    bool operator==(const KlobucharParam &rhs) const
-    {
-        // avoid floating point comparisons by using the raw bits
-        // if rawbits are zero, assume this is a differenced set (operator-)
-        // split up into two 32 bits block, because ulong is only 32 bit
-        return (rawbits.getLeft<0, 32>().to_ulong() != 0)
-                && (rawbits.getLeft<0, 32>().to_ulong() != 0)
-                && (rawbits == rhs.rawbits);
-    }
-
-    bool operator!=(const KlobucharParam &rhs) const
-    {
-        return !(*this == rhs);
-    }
+    bool operator==(const KlobucharParam &rhs) const;
+    bool operator!=(const KlobucharParam &rhs) const;
 };
 
 class Ephemeris
